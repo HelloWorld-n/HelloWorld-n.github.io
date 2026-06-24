22d2147d816062a9e5b32a535340d5b71380d787
HelloWorld-n <gajica.b@gmail.com>
2026-06-24 15:17:21 +0200

    update title

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { createRoot } from "react-dom/client";
 import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 
-import { ColorClock } from "./ColorClock/ColorClock";
+import { ColorClock, getTimezoneName } from "./ColorClock/ColorClock";
+import { getZonedNow } from "./ColorClock/ColorClock";
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
+const title = createRoot(document.querySelector("title") as HTMLElement);
+
+function Title(props = {}) {
+  const [now, setNow] = useState<string>();
+  setInterval(() => {
+    const nowTime = getZonedNow();
+    const nowTosecond = nowTime.toString().split(".")[0];
+    const nowTimezone = getTimezoneName(nowTime);
+    setNow(`${nowTosecond} ${nowTimezone}`);
+  }, 200);
+  return <>{now}</>;
+}
+
+title.render(
+  <HashRouter>
+    <Routes>
+      <Route path="*" element={<Title />} />
+    </Routes>
+  </HashRouter>,
+);
 
 root.render(
   <HashRouter>
@@ -12,7 +33,15 @@ root.render(
     <Routes>
       <Route path="/" element={<Navigate to="/color-clock" />} />
       <Route path="/color-clock" element={<ColorClock />} />
-      <Route path="*" element={<><h1 data-status="404">Not Found</h1></>} />
+      <Route
+        path="*"
+        element={
+          <>
+            <h1 data-status="404">Not Found</h1>
+            <a href="/#/">Return</a>
+          </>
+        }
+      />
     </Routes>
   </HashRouter>,
 );
