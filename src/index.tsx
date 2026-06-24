c424591bf098da53be071d24b8dccc382dd7c347
HelloWorld-n <gajica.b@gmail.com>
2026-06-24 15:33:07 +0200

    simplify title update

--- src/index.tsx
+++ src/index.tsx
@@ -1,47 +1,46 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate, Link } from "react-router-dom";
 
 import { ColorClock, getTimezoneName } from "./ColorClock/ColorClock";
 import { getZonedNow } from "./ColorClock/ColorClock";
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
-const title = createRoot(document.querySelector("title") as HTMLElement);
 
-function Title(props = {}) {
-  const [now, setNow] = useState<string>();
-  setInterval(() => {
-    const nowTime = getZonedNow();
-    const nowTosecond = nowTime.toString().split(".")[0];
-    const nowTimezone = getTimezoneName(nowTime);
-    setNow(`${nowTosecond} ${nowTimezone}`);
-  }, 200);
-  return <>{now}</>;
+function useDynamicTitle() {
+  useEffect(() => {
+    const id = setInterval(() => {
+      const nowTime = getZonedNow();
+      const nowToSecond = nowTime.toString().split(".")[0];
+      const nowTimezone = getTimezoneName(nowTime);
+      document.title = `${nowToSecond} ${nowTimezone}`;
+    }, 200);
+
+    return () => clearInterval(id);
+  }, []);
 }
 
-title.render(
-  <HashRouter>
-    <Routes>
-      <Route path="*" element={<Title />} />
-    </Routes>
-  </HashRouter>,
-);
+function App() {
+  useDynamicTitle();
 
-root.render(
-  <HashRouter>
-    <nav></nav>
-    <Routes>
-      <Route path="/" element={<Navigate to="/color-clock" />} />
-      <Route path="/color-clock" element={<ColorClock />} />
-      <Route
-        path="*"
-        element={
-          <>
-            <h1 data-status="404">Not Found</h1>
-            <a href="/#/">Return</a>
-          </>
-        }
-      />
-    </Routes>
-  </HashRouter>,
-);
+  return (
+    <HashRouter>
+      <nav></nav>
+      <Routes>
+        <Route path="/" element={<Navigate to="/color-clock" />} />
+        <Route path="/color-clock" element={<ColorClock />} />
+        <Route
+          path="*"
+          element={
+            <>
+              <h1 data-status="404">Not Found</h1>
+              <Link to="/">Return</Link>
+            </>
+          }
+        />
+      </Routes>
+    </HashRouter>
+  );
+}
+
+root.render(<App />);
